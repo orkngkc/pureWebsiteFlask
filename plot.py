8932f2c6ba6e2004b04dedc9142d37fd501fb1be
melihkn <melih.sahinbas@sabanciuniv.edu>
2023-08-22 00:31:44 +0300

    selector line at the plot fixed

--- plot.py
+++ plot.py
@@ -6,6 +6,9 @@ tipler = pd.read_excel("Tipler Revize.xlsx")
 tipler["Padişah Dönemi"] = tipler["Padişah Dönemi"].str.strip()
 sultan_list = tipler["Padişah Dönemi"].unique().tolist()
 
+sultan_list.insert(1, "Ertuğrul Gazi")
+sultan_list.pop()
+
 pn.extension("plotly")
 
 
