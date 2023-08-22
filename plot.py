44df1e65717c549fcb3145e92eabc3d5d1681edf
Efe Çınar <111140694+odulsuzkisafilm@users.noreply.github.com>
2023-08-22 04:02:19 +0300

    Added the Pie Charts for objects dataset

--- plot.py
+++ plot.py
@@ -71,7 +71,6 @@ lay2 = pn.Column(king_selector2, responsive2)
 
 ########################################################
 
-
 def update_objects_plot2(selected_king3):
     filtered_data = Eylemler[Eylemler["Sultan"] == selected_king3]
     value_counts = filtered_data["Eylem"].value_counts()
@@ -84,20 +83,82 @@ def update_objects_plot2(selected_king3):
     return fig
 
 king_selector3 = pn.widgets.Select(options=sultan_list2)
-initial_fig3 = update_objects_plot1(king_selector3.value)
-responsive3 = pn.pane.Plotly(initial_fig2, height=600, width=800)
+initial_fig3 = update_objects_plot2(king_selector3.value)
+responsive3 = pn.pane.Plotly(initial_fig3, height=600, width=800)
 
 def selector_callback3(event):
     selected_king3 = event.new
-    updated_fig = update_objects_plot1(selected_king3)
-    responsive3.object = updated_fig  # Fixed: Use responsive2 instead of responsive
+    updated_fig = update_objects_plot2(selected_king3)
+    responsive3.object = updated_fig
 
 king_selector3.param.watch(selector_callback3, "value")
 lay3 = pn.Column(king_selector3, responsive3)
 
+########################################################
+
+def update_objects_pie(selectedEylem):
+    filtered_data = Eylemler[Eylemler["Eylem"] == selectedEylem]
+    value_counts = filtered_data["Sultan"].value_counts()
+
+    fig = px.pie(value_counts, names=value_counts.index, values=value_counts.values,
+                 color='Sultan',
+                 title=f"{selectedEylem} Eylemine Bağlı Nesnelerin Ait Olduğu Padişah Dağılımı",
+                 labels={'value_counts': 'Sultan', "y": "Count", "x": "Sultan"}, height=400)
+
+    return fig
+
+_eylemler = ["Sahiplik", "İsâr", "Ganimet", "Gönderme", "Harac"]
+eylemSelector = pn.widgets.Select(options=_eylemler)
+initial_fig4 = update_objects_pie(eylemSelector.value)
+responsive4 = pn.pane.Plotly(initial_fig4, height=600, width=800)
+
+def selector_callback4(event):
+    selected_eylem = event.new
+    updated_fig = update_objects_pie(selected_eylem)
+    responsive4.object = updated_fig
+
+eylemSelector.param.watch(selector_callback4, "value")
+lay4 = pn.Column(eylemSelector, responsive4)
+
+###########################################################################
+
+def update_objects_pie2(selectedNitelik):
+    filtered_data = Nitelikler[Nitelikler["Nitelik"] == selectedNitelik]
+    value_counts = filtered_data["Sultan"].value_counts()
+
+    fig = px.pie(value_counts, names=value_counts.index, values=value_counts.values,
+                 color='Sultan',
+                 title=f"{selectedNitelik} Niteliğine Bağlı Nesnelerin Ait Olduğu Padişah Dağılımı",
+                 labels={'value_counts': 'Sultan', "y": "Count", "x": "Sultan"}, height=400)
+
+    return fig
+
+_nitelikler = ["Kâlâ", "Taşınılmaz / Toprak", "Seyfiye", "İktidar", "Binek Hayvan", "Deniz Taşıtı", "İş Gücü", "Erzak", "İletişim", "Dini"]
+nitelikSelector = pn.widgets.Select(options=_nitelikler)
+initial_fig5 = update_objects_pie2(nitelikSelector.value)
+responsive5 = pn.pane.Plotly(initial_fig5, height=600, width=800)
+
+def selector_callback5(event):
+    selected_nitelik = event.new
+    updated_fig = update_objects_pie2(selected_nitelik)
+    responsive5.object = updated_fig
+
+nitelikSelector.param.watch(selector_callback5, "value")
+lay5 = pn.Column(nitelikSelector, responsive5)
+
+niteliktab = pn.Tabs(
+    ("Nitelik Dağılımı", lay2),
+    ("Padişah Dağılımı", lay5)
+)
+
+eylemtab = pn.Tabs(
+    ("Eylem Dağılımı", lay3),
+    ("Padişah Dağılımı", lay4)
+)
+
 subtab = pn.Tabs(
-    ("Eyleme Göre", lay3),
-    ("Niteliğe Göre", lay2)
+    ("Eyleme Göre", eylemtab),
+    ("Niteliğe Göre", niteliktab)
 )
 
 tabs = pn.Tabs(
